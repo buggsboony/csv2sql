22a8813478de8334fa504ef60da7a721c9530c87
buggsboony <boonydeboon@gmail.com>
2021-03-31 19:24:53 -0400

    Insert Sql ok

--- gen.php
+++ gen.php
@@ -1,3 +1,16 @@
+<style>
+
+    textarea{
+
+        width : 90%;
+        border:solid 1px #090909;
+        background-color: #090909;
+        color: #A0A0A0;
+        height: 200px;
+    }
+
+</style>
+
 <?php
 
 
@@ -50,11 +63,42 @@ function gen_create($tablename, $columns_infos)
     return $create;
 }//gen_create
 
-
-function gen_insert()
+function getColumns($columns_infos)
 {
+    $columns=array();    
+    foreach($columns_infos as $excel_letter => $infos):
+        //var_dump($excel_letter);
+        //var_dump($infos);
+        $column = $infos->column;
+        $columns[$excel_letter]=$column;
+    endforeach;
+    return $columns;
+}//getColumns
 
 
+function gen_insert( $tablename, $insert_values)
+{
+    if( count( $insert_values ) <0){ echo " Pas de donnée, gen_insert renvoie null."; return null;}
+    $columns = array_keys( $insert_values[0] );
+    $columns_str = "`".implode("`,`",$columns)."`";
+$sql = "/* Insertion de donnée dans la table '$tablename' */
+INSERT INTO `$tablename`
+($columns_str) VALUES
+";
+    //var_dump( $sql ); die("check sql");
+    $values=array();
+    $sql_values = array();
+    foreach($insert_values as $col=>$assoc_arr)
+    {        
+        $row_values = array_values($assoc_arr);
+         $one_row_values = "('". implode("','", $row_values)."')";        
+         //var_dump( $one_row_values ); die("check one_row_values");
+         $sql_values [] = $one_row_values;
+    }//next 
+    $values_str = implode("\n,", $sql_values)."\n";    
+    $sql.=$values_str.";";
+
+    return $sql;
 }//gen_insert
 
 //convert to excel letter
@@ -74,38 +118,68 @@ $creates=array();
 $inserts=array();
 //Parcourir les correspondances
 foreach($correspondances as $tablename => $colsinfo):
- 
     $create_table = gen_create($tablename, $colsinfo);
     echo "create table $tablename: \n ";var_dump($create_table); //die("pause 1 tab");
     $creates[] = $create_table;    
     //Récupérer et ranger par table, la structure pour préparer les colonnes de l'insert
-    $table_columns[]
+    $columns = getColumns($colsinfo); //Out, variable de récupération des colonnes.    
+    $table_columns[$tablename]=$columns;
+
+    //Afficher ça la requete create :
+    echo "<textarea>$create_table</textarea></br>";
 endforeach;
 
-$data_path ="./data";
-$csv_file="GDom.xls"; $tablename = "gdom";
-$sep="\t";
-$fullname=$data_path."/".$csv_file;
-$row = 1;
-if (($handle = fopen($fullname, "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) {
-        // $num = count($data);
-        // //echo "<p> $num champs à la ligne $row: <br /></p>\n";
-        // $row++;
-        
-        // for ($c=0; $c < $num; $c++) {
-        //     //echo $data[$c] . "\n";
+//echo "table cols:"; var_dump( $table_columns ); die("check columns");
 
-        // }
-        foreach( $data as $col_index=>$value):
-            //echo "col_index="; var_dump($col_index);
-            //echo "value="; var_dump($value);
-            $L = excel_letter($col_index);
 
-        endforeach;
-        if($row>=2)break;
-    }
-    fclose($handle);
-}
+function gen_from_csv( $table_columns, $csv_file="GDom.xls", $tablename = "gdom",  $sep="\t", $data_path ="./data")
+{         
+    $fullname=$data_path."/".$csv_file;
+    $row = 1;
+    $columns_htable = $table_columns[$tablename] ;//Récupérer la correspondance Excel => Colonnes nommées
+    $insert_values = array();
+
+    if (($handle = fopen($fullname, "r")) !== FALSE) 
+    {
+        while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) {
+            // $num = count($data);
+            // //echo "<p> $num champs à la ligne $row: <br /></p>\n";
+            // $row++;
+            
+            // for ($c=0; $c < $num; $c++) {
+            //     //echo $data[$c] . "\n";
+
+            // }
+            $datasql = array();
+            foreach( $data as $col_index=>$value):
+                //echo "col_index="; var_dump($col_index);
+                //echo "value="; var_dump($value);
+                $L = excel_letter($col_index+1);
+                //echo "LETTER="; var_dump($L);
+                if( array_key_exists($L, $columns_htable) )
+                {
+                    $colname = $columns_htable[$L];
+                    $datasql[$colname]=$value;                
+                }         
+            endforeach;
+            //echo " data sql:"; var_dump( $datasql );*
+            $insert_values[] = $datasql;
+            //if($row>=1)break;
+        }
+        fclose($handle);
+    } //CSV read
+
+    $sql_insert = gen_insert($tablename, $insert_values);
+
+    //echo "sqlinsert : \n"; var_dump( $sql_insert);
+    echo "<textarea>$sql_insert</textarea></br>\n";
+
+}//gen from csv file
+
+
+
+gen_from_csv( $table_columns, $csv_file="GDom.xls", $tablename = "gdom",  $sep="\t", $data_path ="./data");
+
+gen_from_csv( $table_columns, $csv_file="FCod.xls", $tablename = "fcod",  $sep="\t", $data_path ="./data");
 
 ?>
